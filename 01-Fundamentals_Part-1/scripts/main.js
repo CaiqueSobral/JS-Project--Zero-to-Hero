1819e8ddca6700d23fe549708dc488637c2510e9
Caique Sobral <caiquelsobral@gmail.com>
2022-03-27 08:47:53 -0300

    feat: Variables and its rules

--- 01-Fundamentals_Part-1/scripts/main.js
+++ 01-Fundamentals_Part-1/scripts/main.js
@@ -1,4 +1,32 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun!");
+let country = "Brazil";
+let continent = "South America";
+let population = "200.4 million";
 
-console.log(40 + 8 + 23 - 10);
+console.log(
+  `${country} is in ${continent} and has o population of ${population}`
+);
+
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Bob";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "Jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrantJob = "Teacher";
+
+// let job1 = "Programmer";
+// let job2 = "Teacher";
+// console.log(myFirstJob);
