4ffe8825faf80ce4e7dd1eb221abcd2a9b6455ac
Caique Sobral <caiquelsobral@gmail.com>
2022-03-27 09:40:16 -0300

    feat: Basic operators

--- 01-Fundamentals_Part-1/scripts/main.js
+++ 01-Fundamentals_Part-1/scripts/main.js
@@ -1,63 +1,90 @@
-// let country = "Brazil";
-// let continent = "South America";
-// let population = "200.4 million";
+/* let country = "Brazil";
+let continent = "South America";
+let population = "200.4 million";
 
-// console.log(
-//   `${country} is in ${continent} and has o population of ${population}`
-// );
+console.log(
+  `${country} is in ${continent} and has o population of ${population}`
+);
 
-// let js = "amazing";
-// console.log(40 + 8 + 23 - 10);
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
 
-// console.log("Jonas");
-// console.log(23);
+console.log("Jonas");
+console.log(23);
 
-// let firstName = "Bob";
+let firstName = "Bob";
 
-// console.log(firstName);
-// console.log(firstName);
-// console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
-// let jonas_matilda = "JM";
-// let $function = 27;
+let jonas_matilda = "JM";
+let $function = 27;
 
-// let person = "Jonas";
-// let PI = 3.1415;
+let person = "Jonas";
+let PI = 3.1415;
 
-// let myFirstJob = "Programmer";
-// let myCurrantJob = "Teacher";
+let myFirstJob = "Programmer";
+let myCurrantJob = "Teacher";
 
-// let job1 = "Programmer";
-// let job2 = "Teacher";
-// console.log(myFirstJob);
+let job1 = "Programmer";
+let job2 = "Teacher";
+console.log(myFirstJob);
 
-// let javascriptIsFun = true;
-// console.log(typeof javascriptIsFun);
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
 
-// console.log(typeof true);
-// console.log(typeof 23);
-// console.log(typeof "Jonas");
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Jonas");
 
-// javascriptIsFun = "Yes";
-// console.log(typeof javascriptIsFun);
+javascriptIsFun = "Yes";
+console.log(typeof javascriptIsFun);
 
-// let year;
-// console.log(year);
-// console.log(typeof year);
+let year;
+console.log(year);
+console.log(typeof year);
 
-// year = 1991;
-// console.log(year);
-// console.log(typeof null);
+year = 1991;
+console.log(year);
+console.log(typeof null);
 
 let agr = 30;
 age = 31;
 
 const birthYear = 1991;
-// birthYear = 1990;
-// const job;
+birthYear = 1990;
+const job;
 
 var job = "Programmer";
 job = "teacher";
 
 lastName = "Sobral";
 console.log(lastName);
+*/
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Caique";
+const lastName = "Sobral";
+
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; //15
+x += 10; // x = x + 10 - 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+
+console.log(now - 1991 > now - 2028);
