77bd0bad079109e63b2f41df122617cdac16b17b
Caique Sobral <caiquelsobral@gmail.com>
2022-03-28 14:36:19 -0300

    feat: arrow functions

---
+++ 02-Fundamentals_Part-2/scripts/04-arrow-functions.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const calcAge3 = (birthYear) => 2022 - birthYear;
+const age3 = calcAge3(2001);
+console.log(age3);
+
+const yearsUntilRetirement = (firstName, birthYear) => {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement("Caique", 2001));
+console.log(yearsUntilRetirement("Jonas", 1991));
