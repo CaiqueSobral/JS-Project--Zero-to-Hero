56d68cb6f104a3cb1607fdbe05e09248c4b73088
Caique Sobral <caiquelsobral@gmail.com>
2022-03-29 16:56:16 -0300

    feat: get element by classes and create function to hide the dice

--- 03-DOM-Manipulation/Pig-Game/scripts/main.js
+++ 03-DOM-Manipulation/Pig-Game/scripts/main.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+// Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const diceEl = document.querySelector(".dice");
+
+const resetGame = function () {
+  diceEl.classList.add("hidden");
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+};
+resetGame();
