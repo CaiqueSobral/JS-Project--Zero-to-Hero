f934f05ff76f8c0ae906d34e3d3ee71ae5092ae1
Caique Sobral <caiquelsobral@gmail.com>
2022-03-28 23:18:22 -0300

    feat: introduction to objects

---
+++ 02-Fundamentals_Part-2/scripts/11-Introduction-to-objects.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// ARRAY
+const caiqueArray = [
+  "Caique",
+  "Sobral",
+  2022 - 2001,
+  "Programmer",
+  ["Japa", "Kaue", "Marcos"],
+];
+
+// OBJECT
+const caique = {
+  firstName: "Caique",
+  lastName: "Sobral",
+  age: 2022 - 2001,
+  job: "Programmer",
+  friends: ["Japa", "Kaue", "Marcos"],
+};
+
+console.log(caique);
