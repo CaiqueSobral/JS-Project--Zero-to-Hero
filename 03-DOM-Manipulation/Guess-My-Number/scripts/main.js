0cdcc262051e301f8b52e9538b2e43885a6cbfd6
Caique Sobral <caiquelsobral@gmail.com>
2022-03-29 12:58:49 -0300

    feat: coding challenge #01

--- 03-DOM-Manipulation/Guess-My-Number/scripts/main.js
+++ 03-DOM-Manipulation/Guess-My-Number/scripts/main.js
@@ -1,10 +1,10 @@
 "use strict";
 
 let score = 10;
+let highScore = 0;
 document.querySelector(".score").textContent = score;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -15,9 +15,13 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent =
       "THAT'S THE NUMBER!!! CONGRATS!!!";
-
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high...";
@@ -37,4 +41,20 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You lost!";
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  //RESET NUMBERS
+  score = 10;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  //RESET THE TEXTS
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+
+  //RESET STYLES
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
