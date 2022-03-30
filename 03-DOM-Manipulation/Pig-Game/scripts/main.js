fcc49be792edbc7aa463bb7dec80cd87e56cf799
Caique Sobral <caiquelsobral@gmail.com>
2022-03-30 09:49:21 -0300

    feat: switching players

--- 03-DOM-Manipulation/Pig-Game/scripts/main.js
+++ 03-DOM-Manipulation/Pig-Game/scripts/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 const current0El = document.querySelector("#current--0");
@@ -9,7 +11,10 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 const resetGame = function () {
   diceEl.classList.add("hidden");
@@ -29,9 +34,15 @@ btnRoll.addEventListener("click", function () {
   //3 Check if rolled 1: if true change player
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     //ADD dice to the current score
   } else {
     //Switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
