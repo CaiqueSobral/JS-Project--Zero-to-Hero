f671e9cc4f5e40ac4d924327f1fd1bd8cfe50208
Caique Sobral <caiquelsobral@gmail.com>
2022-04-04 18:01:35 -0300

    feat: Logical assignment operators

--- 05-Data-Structures-Modern-Operators-and-Strings/scripts/07-logical-assignment-operators.js
+++ 05-Data-Structures-Modern-Operators-and-Strings/scripts/07-logical-assignment-operators.js
@@ -46,3 +46,33 @@ const restaurant = {
     console.log(mainIngredient, otherIngridients);
   },
 };
+
+const rest1 = {
+  name: "Capri",
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "La piazza",
+  owner: "Giovanni Rossi",
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// NULLISH assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+rest1.owner = rest1.name && "<ANONYMOUS>";
+rest2.owner = rest2.name && "<ANONYMOUS>";
+
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
