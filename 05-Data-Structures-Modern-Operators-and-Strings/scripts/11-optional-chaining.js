29dd6a1215ef33a78b0520bcfa197d3243045e12
Caique Sobral <caiquelsobral@gmail.com>
2022-04-06 08:59:13 -0300

    feat: optional chaining

--- 05-Data-Structures-Modern-Operators-and-Strings/scripts/11-optional-chaining.js
+++ 05-Data-Structures-Modern-Operators-and-Strings/scripts/11-optional-chaining.js
@@ -47,3 +47,28 @@ const restaurant = {
     console.log(mainIngredient, otherIngridients);
   },
 };
+
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? "Closed";
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+// Arryas
+const users = [{ name: "Caique", email: "Caique@gmail.com" }];
+console.log(users[0]?.name ?? "User array empty");
+
+const usersEmpty = [];
+console.log(usersEmpty[0]?.name ?? "User array empty");
