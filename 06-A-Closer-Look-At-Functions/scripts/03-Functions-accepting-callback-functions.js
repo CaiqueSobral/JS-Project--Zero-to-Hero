ff58a5960003d7bc00c051652752e055512565da
Caique Sobral <caiquelsobral@gmail.com>
2022-05-23 14:27:43 -0300

    Feat: Functions accepting callback functions

---
+++ 06-A-Closer-Look-At-Functions/scripts/03-Functions-accepting-callback-functions.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋🏻');
+};
+document.body.addEventListener('click', high5);
+
+['Caique', 'Suxu', 'Caique e Suxu'].forEach(high5);
