6df2d3e0401c46e23620aab72db042341eeab4f2
Caique Sobral <caiquelsobral@gmail.com>
2022-08-04 16:32:40 -0300

    Feat: The reminder operator

---
+++ 08-Numbers-Dates-Intl-and-Timers/scripts/03-The-Reminder-Operator.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 7);
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
