372000930e6c458e807b8340eac91b69b9eea644
Caique Sobral <caiquelsobral@gmail.com>
2022-08-04 10:44:09 -0300

    Feat: Implementing Transfers

--- 07-Working-With-Arrays/scripts/01-Simple-array-methods.js
+++ 07-Working-With-Arrays/scripts/01-Simple-array-methods.js
@@ -8,6 +8,7 @@ console.log(arr.slice(-1));
 console.log(arr.slice(1, -2));
 console.log(arr.slice());
 console.log([...arr]);
+console.log()
 
 // SPLICE method -- Mutate the original array
 // console.log(arr.splice(2));
@@ -22,10 +23,11 @@ const arr2 = ['j', 'i', 'h', 'g', 'f'];
 console.log(arr2.reverse());
 console.log(arr2);
 
-// CONCAT
+// CONCAT"
 const letters = arr.concat(arr2);
 console.log(letters);
 console.log([...arr, ...arr2]); // Does the same thing
 
 // JOIN
 console.log(letters.join(' - '));
+('');
