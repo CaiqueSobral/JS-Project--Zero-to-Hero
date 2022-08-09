f56d5cf5d1b43078bbd64fc87982bb4fc02434af
Caique Sobral <caiquelsobral@gmail.com>
2022-08-09 12:28:29 -0300

    Feat: Passing arguments to event handlers

--- 09-Advanced-DOM-and-Events/scripts/main.js
+++ 09-Advanced-DOM-and-Events/scripts/main.js
@@ -6,6 +6,7 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
 
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
@@ -36,6 +37,31 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//==========================================================
+//==========================================================
+
+////////////////////////////////////////////////////////////
+//////////////////  MENU FADE ANIMATION  ///////////////////
+////////////////////////////////////////////////////////////
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+// Opposite of Mouse Over
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 //==========================================================
 //==========================================================
