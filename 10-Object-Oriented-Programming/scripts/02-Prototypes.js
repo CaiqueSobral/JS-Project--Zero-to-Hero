9430424557aa7c5070c9c831b42bc11ce84933f8
Caique Sobral <caiquelsobral@gmail.com>
2022-08-10 11:53:00 -0300

    Feat: Prototypes

--- 10-Object-Oriented-Programming/scripts/02-Prototypes.js
+++ 10-Object-Oriented-Programming/scripts/02-Prototypes.js
@@ -7,26 +7,9 @@
 const Person = function (firstName, birthear) {
   this.firstName = firstName;
   this.birthear = birthear;
-
-  // Never create methods in constructor functions
-  // this.calcAge = function () {
-  //   console.log(2022 - this.birthear);
-  // };
 };
 
 const caique = new Person('Caique', 2001);
-const japa = new Person('Japa', 1998);
-const kassino = new Person('Kassino', 1998);
-const jonas = new Person('Jonas', 2010);
-
-console.log(kassino, caique);
-// {} = object
-// 1. New {} is created
-// 2. Function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
-
-console.log(caique instanceof Person); // True
 
 //==========================================================
 //==========================================================
@@ -42,12 +25,11 @@ Person.prototype.calcAge = function () {
 };
 
 caique.calcAge();
-japa.calcAge();
 
 console.log(caique.__proto__);
 console.log(caique.__proto__ === Person.prototype); // True
 
 Person.prototype.species = 'Homo Sapiens';
-console.log(caique.species, kassino.species);
+console.log(caique.species);
 //==========================================================
 //==========================================================
