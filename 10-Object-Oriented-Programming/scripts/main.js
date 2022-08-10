da45533487d53790b3914edde4150d04beee15f9
Caique Sobral <caiquelsobral@gmail.com>
2022-08-10 11:36:11 -0300

    Feat: Constructor functions

---
+++ 10-Object-Oriented-Programming/scripts/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Person = function (firstName, birthear) {
+  this.firstName = firstName;
+  this.birthear = birthear;
+
+  // Never create methods in constructor functions
+  // this.calcAge = function () {
+  //   console.log(2022 - this.birthear);
+  // };
+};
+
+const caique = new Person('Caique', 2001);
+const japa = new Person('Japa', 1998);
+const kassino = new Person('Kassino', 1998);
+const jonas = new Person('Jonas', 2010);
+
+console.log(kassino, caique);
+// {} = object
+// 1. New {} is created
+// 2. Function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+console.log(caique instanceof Person); // True
