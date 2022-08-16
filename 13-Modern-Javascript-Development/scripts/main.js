8c46a897440c924d2b6dabfeb56c05232e1a62ad
Caique Sobral <caiquelsobral@gmail.com>
2022-08-16 13:01:03 -0300

    FEAT: Introduction to NPM

--- 13-Modern-Javascript-Development/scripts/main.js
+++ 13-Modern-Javascript-Development/scripts/main.js
@@ -8,33 +8,50 @@
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 
-console.log('Importing module');
+// console.log('Importing module');
 
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 4);
-add('olives', 8);
-add('apples', 5);
-add('bananas', 1);
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 4);
+// add('olives', 8);
+// add('apples', 5);
+// add('bananas', 1);
 
-console.log(cart);
+// console.log(cart);
 
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts'); // Blocks the execution
 // const data = await res.json();
 // console.log(data);
 
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts'); // Blocks the execution
-  const data = await res.json();
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts'); // Blocks the execution
+//   const data = await res.json();
 
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
+// const lastPost = getLastPost();
 // console.log(lastPost);
 
 // Not very clean
 //lastPost.then((val) => console.log(val));
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 2 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
